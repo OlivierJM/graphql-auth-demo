2964215efe8e4e247125b03ac9d0729e936cbbaa
Olivier JM <manolivier93@gmail.com>
2019-05-27 10:12:12 +0200

    added bcrypt and configured hashing

--- index.js
+++ index.js
@@ -2,7 +2,17 @@
 // import jwt from "jsonwebtoken";
 const { ApolloServer, gql } = require("apollo-server");
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 // Construct a schema, using GraphQL schema language
+
+const userSchema = mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const User = mongoose.model("User", userSchema);
+
 const typeDefs = gql`
   type Query {
     user: User
@@ -26,13 +36,34 @@ const resolvers = {
     }
   },
   Mutation: {
-    register: (root, args) => {
+    register: async (root, args) => {
+      const user = User();
+      user.email = args.email;
+      user.password = await bcrypt.hash(args.password, 12);
       // we will get the email and password from the args object
-      console.log(args);
-      return args.password;
+      // console.log(args);
+      return user.save();
     },
-    login: (root, args, context) => {
+    login: async (root, args, context) => {
       // we will generate a token for the user here
+      const user = await User.findOne({ email: context.email });
+      if (!user) {
+        throw new Error("No user found ");
+      }
+      const isValid = await bcrypt.compare(context.password, user.password);
+      if (!isValid) {
+        throw new Error("Incorrect password ");
+      }
+      //   sign in the user
+      const token = await jwt.sign(
+        {
+          user: pick(user, ["_id", "email"])
+        },
+        SECRET,
+        // this token will last for a year, this should be adjusted accordingly
+        { expiresIn: "1y" }
+      );
+      return token;
       console.log(args);
       return context.token;
     }
@@ -46,13 +77,15 @@ const server = new ApolloServer({
     let user;
     try {
       user = await jwt.verify(token, SECRET);
-      console.log(user.user);
+      console.log(`${user.user} user`);
+      // user = await jwt.verify(token, SECRET);
+      // console.log(`${user.user} user`);
     } catch (error) {
       console.log(`${error.message} caught`);
     }
     // the user and secret we are passing here is what we access in every resolver
     return {
-      user,
+      user: await user,
       SECRET
     };
   }
